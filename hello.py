30d525e141531b36fc410524f97c5a7a644c27c7
enkeljon gjeta <enkelionimarku@gmail.com>
2025-01-02 14:03:06 +0100

    made a 404 and 500 html files

--- hello.py
+++ hello.py
@@ -14,3 +14,15 @@ def index():
 
 def user(name):
     return render_template("user.html", user_name=name)
+
+#invalid url
+@app.errorhandler(404)
+
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#eternal server error
+@app.errorhandler(500)
+
+def page_not_found(e):
+    return render_template("500.html"), 500
