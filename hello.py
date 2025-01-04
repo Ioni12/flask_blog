1753b1964261f57f1610ee0a6131e9789a3deac0
enkeljon gjeta <enkelionimarku@gmail.com>
2025-01-04 16:56:42 +0100

    soaking more knowledge

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -34,3 +34,14 @@ def page_not_found(e):
 
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route("/name", methods=["GET", "POST"])
+
+def name():
+    name = None
+    form = naming()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("form submitted succesfuly")
+    return render_template("name.html", name=name, form=form)
